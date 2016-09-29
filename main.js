db83f72682cc59e21a835f02441564cab944449a
Robert Zehnder <robzcodes@gmail.com>
2016-09-29 16:10:34 -0400

    Made final updates to board game. These changes include what was requested in pre work doc + clear button

--- main.js
+++ main.js
@@ -4,42 +4,98 @@ var cards = ['queen', 'queen','king','king'];
 
 var cardsInPlay = [];
 
-
-function createCards() {
-	
-	var board = document.getElementById('game-board');
+//var x =document.getElementById('clearButton');//.addEventListener('click',clearCards());
 
 
-	for (i = 0; i < 4; i++) {
-		var newCard = document.createElement('div');
-		newCard.className = 'card';
-		board.appendChild(newCard);
-
-	}
-
-}
-
-createCards();
 
 function createBoard() {
 
+	var board = document.getElementById('game-board');
+
 	for (var i = 0; i< cards.length; i++) {
-		cardElement.setAttribute('data-card', cards[i]);
+		
+		//Adding the clear div, so that the cards stack in rows of 2
+
+		if (i === 2) {
+			var clearDiv = document.createElement('div');
+			clearDiv.className = 'clear';
+			board.appendChild(clearDiv);
+		}
+
+		//create new card
+
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+
+		//setting the attribute of the card to it's spot in the array
+
+		newCard.setAttribute('data-card', cards[i]);
 
 		// when a card is clicked the function isTwoCards will be executed
-    	cardElement.addEventListener('click', isTwoCards);
+    	newCard.addEventListener('click', isTwoCards);
 
-		cardElement.innerHTML = '<img src="pictures/king.png" alt="King of Spades" />';
-
+    	board.appendChild(newCard);
 
 	}
 }
 
 createBoard();
 
-function isMatch() {
+
+function isTwoCards() {
 	
-	if (cardOne === cardFour) {
+	cardsInPlay.push(this.getAttribute('data-card'));
+
+
+	console.log(this.getAttribute('data-card'));
+
+	if (this.getAttribute('data-card') === "king") {
+		this.innerHTML = '<img src="pictures/king.png" alt="King of Spades" />';
+	}
+
+	else if (this.getAttribute('data-card') === "queen") {
+		this.innerHTML = '<img src="pictures/queen.png" alt="King of Spades" />';
+	}
+
+  	// if you have two cards in play check for a match
+  	if (cardsInPlay.length === 2) {
+
+  	
+
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+
+
+
+    // clear cards in play array for next try
+    cardsInPlay = [];
+
+
+
+  }
+
+  //clearCards(); //Clear pictures from board for retry
+
+
+}
+
+
+
+    
+
+
+var potato = function clearCards() {
+
+	var cardHolder = document.querySelectorAll(".card");
+	
+	for (i = 0; i < cardHolder.length; i++) {
+		cardHolder[i].innerHTML = "";
+	}
+}
+
+function isMatch(cardsInPlay) {
+	
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	}
 
@@ -49,18 +105,6 @@ function isMatch() {
 
 }
 
-function isTwoCards() {
-	cardsInPlay.push(this.getAttribute('data-card'));
+var button = document.querySelector('button');
+	button.addEventListener('click', potato); 
 
-  // if you have two cards in play check for a match
-  if (cardsInPlay.length === 2) {
-
-    // pass the cardsInPlay as an argument to isMatch function
-    isMatch(cardsInPlay);
-
-    // clear cards in play array for next try
-    cardsInPlay = [];
-
-  }
-
-}
